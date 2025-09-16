1a84b216204a91db8036c728b7fecf08d3e04c76
TriNgCao <tringuyencao.se@gmail.com>
2025-09-16 21:39:46 +0700

    initial project

---
+++ backend/src/controllers/tasksControllers.js
@@ -0,0 +1,55 @@
+import Task from '../models/Task.js';
+
+export const getAllTasks = async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    }
+    catch (error) {
+        console.error("Error fetching tasks:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const createTask = async (req, res) => {
+    try {
+        const { title } = req.body;
+        const newTask = new Task({ title });
+        await newTask.save();
+        res.status(201).json(newTask);
+    } catch (error) {
+        console.error("Error creating tasks:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const updateTask = async (req, res) => {
+    try {
+        const { title, status, completedAt } = req.body;
+        const updatedTask = await Task.findByIdAndUpdate(
+            req.params.id,
+            { title, status, completedAt },
+            { new: true }
+        );
+        if (!updatedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json(updatedTask);
+    } catch (error) {
+        console.error("Error updating tasks:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+export const deleteTask = async (req, res) => {
+    try {
+        const deletedTask = await Task.findByIdAndDelete(req.params.id);
+        if (!deletedTask) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+        res.status(200).json({ message: "Task deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting tasks:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+}
